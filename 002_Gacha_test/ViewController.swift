4bcc0f226218978219f6b07d7976f3ec60a68f53
yukiees <yukiii8989@gmail.com>
2020-11-12 09:27:51 +0900

    Initial Commit

---
+++ 002_Gacha_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  002_Gacha_test
+//
+//  Created by 松島優希 on 2020/11/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
